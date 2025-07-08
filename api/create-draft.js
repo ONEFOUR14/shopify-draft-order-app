308a10a6de2eadb06b3a6017effff46c60375829
ONEFOUR14 <3842643@gmail.com>
2025-07-08 13:22:30 +0800

    Update create-draft.js

--- api/create-draft.js
+++ api/create-draft.js
@@ -2,21 +2,30 @@ const { Shopify } = require('@shopify/shopify-api');
 
 module.exports = async (req, res) => {
   const cart = req.body.cart;
+  const shop = process.env.SHOP.replace('https://', '');
+  const token = process.env.SHOPIFY_ADMIN_TOKEN;
 
-  const session = await Shopify.Utils.loadOfflineSession(process.env.SHOP);
+  const client = new Shopify.Clients.Rest(shop, token);
 
-  const lineItems = cart.items.map((item) => ({
+  const lineItems = cart.items.map(item => ({
     variant_id: item.variant_id,
     quantity: item.quantity
   }));
 
-  const draftOrder = new Shopify.Rest.DraftOrder({ session });
-  draftOrder.line_items = lineItems;
-  draftOrder.note = 'Auto-created when entering checkout';
-  draftOrder.tags = ['auto-created'];
+  const response = await client.post({
+    path: 'draft_orders',
+    data: {
+      draft_order: {
+        line_items: lineItems,
+        note: 'Auto-created when entering checkout',
+        tags: ['auto-created']
+      }
+    },
+    type: 'json'
+  });
 
-  await draftOrder.save();
+  const draftOrderId = response.body.draft_order.id;
 
-  res.cookie('draft_order_id', draftOrder.id, { httpOnly: true });
-  res.status(200).json({ success: true, draft_order_id: draftOrder.id });
+  res.cookie('draft_order_id', draftOrderId, { httpOnly: true });
+  res.status(200).json({ success: true, draft_order_id: draftOrderId });
 };
